1df8a83e7bceadb350061b44a06a02a40e4bfd32
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-11-01 11:21:13 -0600

    Update parties migration and schema

--- db/migrate/20231101014117_create_parties.rb
+++ db/migrate/20231101014117_create_parties.rb
@@ -2,13 +2,13 @@ class CreateParties < ActiveRecord::Migration[7.0]
   def change
     create_table :parties do |t|
       t.string :access_code
-      t.integer :max_rating, default: -1
-      t.integer :max_duration, default: -1
+      t.integer :max_rating, default: nil
+      t.integer :max_duration, default: nil
       t.string :genres
       t.string :services
       t.string :languages
       t.integer :format
-      t.integer :movie_id, default: 0
+      t.integer :movie_id, default: nil
 
       t.timestamps
     end
