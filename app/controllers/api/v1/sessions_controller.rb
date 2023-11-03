0e0eeaa507a93d1001a374d5c8be461f95c34d08
Joop Stark <stark.joop@gmail.com>
2023-11-02 22:23:43 -0600

    refactored sessions service

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password_digest])
+    if user.authenticate(params[:password])
       render json: {id: user.id}, status: :created
     else
       render json: {id: "bad cred"}, status: 401
