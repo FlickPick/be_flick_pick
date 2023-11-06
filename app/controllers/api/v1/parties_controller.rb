4079151107f28b65596f9e789edf66d27a7f10a8
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-11-06 12:00:32 -0700

    Complete testing

--- app/controllers/api/v1/parties_controller.rb
+++ app/controllers/api/v1/parties_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::PartiesController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_record_invalid
+
   def index
     render json: PartySerializer.new(Party.all)
   end
@@ -12,27 +14,21 @@ class Api::V1::PartiesController < ApplicationController
     trailer_data = MoviesService.new.trailers(movie_id)
     if movie_data
       render json: {
-      party: PartySerializer.new(party),
-      movie: {
-        original_title: movie_data[:original_title],
-        poster_path: movie_data[:poster_path] 
-      },
-      cast: make_cast(cast_data),
-      trailer: Trailer.new(trailer_data)
-    }
-    else
-      render json: { error: 'No movie data' }, status: :not_found
+        party: PartySerializer.new(party),
+        movie: {
+          original_title: movie_data[:original_title],
+          poster_path: movie_data[:poster_path] 
+        },
+        cast: make_cast(cast_data),
+        trailer: Trailer.new(trailer_data)
+      }
     end
   end
 
   def create
     party = Party.create!(party_params)
-    if party.save
-      # create_host(party)
-      render json: PartySerializer.new(party), status: :created
-    else
-      head 401
-    end
+    party.save
+    render json: PartySerializer.new(party), status: :created
   end
 
   def update
@@ -54,13 +50,17 @@ class Api::V1::PartiesController < ApplicationController
     params.require(:party).permit(:max_rating, :max_duration, :genres, :services, :movie_id) 
   end
 
-  def create_host(party)
-    UserParty.create(party: party, user_id: params[:user_id], host: true)
-  end
+  # def create_host(party)
+  #   UserParty.create(party: party, user_id: params[:user_id], host: true)
+  # end
 
   def make_cast(cast_data)
-    cast_data[:cast].map do |cast_member|
+    cast_data.map do |cast_member|
       Cast.new(character: cast_member[:character], name: cast_member[:name])
     end
+  end
+
+  def handle_record_invalid(exception)
+    render json: { error: exception.message }, status: 401
   end
 end
