194f42de2e194ac741d1230f5c0aabff7d397c31
Joop Stark <stark.joop@gmail.com>
2023-11-06 11:40:14 -0700

    modified movie index to return array

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::MoviesController < ApplicationController
     render json: MovieSerializer.new(make_movie(MoviesService.new.movie(params[:movie_id])))
   end
 
-  def index(party_id, round)
-    render json: MovieSerializer.new(round_movies(party_id, round))
+  def index
+    render json: round_movies(params[:party_id], params[:round])
   end
 
   private
@@ -14,12 +14,12 @@ class Api::V1::MoviesController < ApplicationController
   end
 
   def round_movies(party_id, round)
-    find_round_movies(party_id, round).map do |movie_data|
-      make_movie(movie_data)
+    find_round_movies(party_id, round)[:results].map do |data|
+      data[:id]
     end
   end
 
   def find_round_movies(party_id, round)
-    MovieService.new.movies_by_round(party_id, round)
+    MoviesService.new.movies_by_round(party_id, round)
   end
-end
+end 
